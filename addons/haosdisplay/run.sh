5cc7011e849715ea369b7aef08e505726a713e9c
atila593 <108370757+atila593@users.noreply.github.com>
2025-11-26 22:34:21 +0100

    Update run.sh

--- addons/haosdisplay/run.sh
+++ addons/haosdisplay/run.sh
@@ -5,6 +5,7 @@ echo "######## Starting HAOS Kiosk Mod - Firefox ########"
 # Options depuis config.json
 OPTIONS_FILE="/data/options.json"
 
+# Récupération des variables avec jq (assurez-vous d'utiliser la syntaxe correcte de jq)
 HA_URL=$(jq -r '.ha_url' "$OPTIONS_FILE")
 DASHBOARD=$(jq -r '.ha_dashboard' "$OPTIONS_FILE")
 LOGIN_DELAY=$(jq -r '.login_delay' "$OPTIONS_FILE")
@@ -19,7 +20,9 @@ export DISPLAY=:0
 sleep "$LOGIN_DELAY"
 
 # Lancer Firefox en mode kiosque
-firefox-esr --kiosk "$HA_URL/$DASHBOARD" --width 1920 --height 1080 &
+# NOTE: Nous utilisons la syntaxe 'sh' pour garantir la compatibilité
+firefox-esr --kiosk "$HA_URL/$DASHBOARD" &
 
 # Garder le conteneur actif, en Remplaçant le Shell par 'tail' (devient PID 1)
+# C'EST LA LIGNE CRUCIALE POUR S6
 exec tail -f /dev/null
